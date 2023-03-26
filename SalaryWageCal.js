85a9dc359345004329931b9ae4a863e3ff96ce30
Oyinlolatechie <Adepitanoyinlola4@gmail.com>
2023-03-26 14:28:59 +0100

    feat: Add function to validate user inputs

--- SalaryWageCal.js
+++ SalaryWageCal.js
@@ -1,30 +1,55 @@
+function validateUserInput(employeeType, salesPerMonth){
+
+    if (!employeeType || !salesPerMonth){
+     throw new Error ("Please enter value for employeeType and salesPerMonth")
+    }
+    
+   
+
+    if(typeof(employeeType) !== 'string'){
+        throw new Error ("EmployeeType must be a string")
+    }
+ 
+    if(typeof(salesPerMonth) !== 'number'|| isNaN(salesPerMonth)){
+      throw new Error('SalesPerMonth, must be a number')
+    }
+
+    if(salesPerMonth < 0){
+        throw new Error ("SalesPermonth cant be less than 0")
+    }
+}
+
+
+
 function takeHomeSalary(employeeType, salesPerMonth){
+
+    validateUserInput(employeeType, salesPerMonth)
+
     //set global variables
-    let salary 
+    let basedSalary 
     let commission
     let totalSalary
-    
 
-    switch (employeeType) {
+    switch (employeeType.toUpperCase()) {
         case "SALARIED":
-            salary = 150000; //Based salary
-            commission = 0; // since based employees have no commissiomn
-            totalSalary = salary + commission
-            return totalSalary
+            basedSalary = 150000;
+            commission = 0; // since based employees have no commissin
+            totalSalary = basedSalary + commission
+            return `#${totalSalary}`
             break;
 
         case "COMMISSIONED":
-            salary = 80000; //Based salary
+            basedSalary = 80000; 
              commission = (8.5 * salesPerMonth)/100
-             totalSalary = salary + commission
-             return totalSalary;
+             totalSalary = basedSalary + commission
+             return `#${totalSalary}`;
             break;
 
         case "CONTRACT":
-             salary= 0;
+            basedSalary= 0; // contract staff has no based salary
              commission = (40 * salesPerMonth)/100
-             totalSalary = salary + commission
-             return totalSalary;
+             totalSalary = basedSalary + commission
+             return `#${totalSalary}`;
             break;
 
         default:
@@ -33,5 +58,10 @@ function takeHomeSalary(employeeType, salesPerMonth){
     }
 }
 
-// let checkSalary = takeHomeSalary("CONTRACT", 500) 
-// console.log(checkSalary)
+try {
+    let checkSalary = takeHomeSalary('contract', 1) 
+    console.log(checkSalary)  
+} catch (error) {
+    console.log(error.message)
+} 
+
