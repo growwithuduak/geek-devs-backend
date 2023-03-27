91337acdc797c0434669cb81b9f2577d1826afa4
nelo_olikeze <neloolikeze@gmail.com>
2023-03-27 07:51:52 +0100

    refactor: Refactor code to improve readability and clarity
    Restructured the code to improve readability by using more descriptive variable names and separating functions to handle one concern each. This is to make the code easier to read and understand, improving its maintainability and reducing the chances of errors.

--- index.js
+++ index.js
@@ -1,23 +1,66 @@
-function salesPerMonthValidation(salesPerMonth) {
-    if (isNaN(salesPerMonth) || salesPerMonth < 0) {
-        throw new Error('Please enter the valid amount generated for the month');
+const employeeTypes = {SALARIED : 'SALARIED', COMMISSIONED : 'COMMISSIONED',CONTRACT : 'CONTRACT'}
+
+const employeeSalaryParameters = {
+    BASE_SALARIED: 150000,
+    BASE_COMMISSIONED: 80000,
+    PERCENTAGE_COMMISIONED: 0.085,
+    PERCENTAGE_CONTRACT: 0.4
+}
+
+const errorMessages = {
+    invalid_employee_type: "Please enter a valid employee type",
+    invalid_salesPerMonth: "Please enter a valid amount generated for the month"
+}
+
+const employeeTypeValidation = (employeeType) => {
+    if(!employeeTypes.hasOwnProperty(employeeType)){
+        throw new Error(errorMessages.invalid_employee_type)
     }
 }
 
-function calculateEmployeeSalary(employeeType, salesPerMonth) {
-    let monthlySalary;
-    let commissionedPay = 80000 + (0.085 * salesPerMonth)
-    let contractPay = 0.4 * salesPerMonth;
-
-    if (employeeType === 'SALARIED') {
-        return(monthlySalary = 150000);
-    } else if (employeeType === 'COMMISSIONED') {
-        salesPerMonthValidation(salesPerMonth);
-        return(monthlySalary = commissionedPay);
-    } else if (employeeType === 'CONTRACT') {
-        salesPerMonthValidation(salesPerMonth);
-        return(monthlySalary = contractPay)
-    } else {
-        throw new Error('Please enter a valid employee type');
+const salesPerMonthValidation = (salesPerMonth) => {
+    if(typeof(salesPerMonth) !== 'number' || salesPerMonth < 0){
+        throw new Error(errorMessages.invalid_salesPerMonth)
     }
 }
+
+const calculateAllTypesOfEmployeeSalary = (baseSalary, percentage, salesPerMonth) =>{
+    return baseSalary + (percentage * salesPerMonth)
+}
+
+const calculateSalariedEmployeeSalary = () => {
+    return calculateAllTypesOfEmployeeSalary(employeeSalaryParameters.BASE_SALARIED, 0, 0)
+}
+
+const calculateCommissionedEmployeeSalary = (salesPerMonth) => {
+    salesPerMonthValidation(salesPerMonth)
+    return calculateAllTypesOfEmployeeSalary(employeeSalaryParameters.BASE_COMMISSIONED, employeeSalaryParameters.PERCENTAGE_COMMISIONED, salesPerMonth)
+}
+
+const calculateContractEmployeeSalary = (salesPerMonth) => {
+    salesPerMonthValidation(salesPerMonth)
+    return calculateAllTypesOfEmployeeSalary(0, employeeSalaryParameters.PERCENTAGE_CONTRACT, salesPerMonth)
+}
+
+const calculateEmployeeSalary = (employeeType, salesPerMonth) => {
+    employeeTypeValidation(employeeType)
+
+    if(employeeType === employeeTypes.SALARIED){
+        return calculateSalariedEmployeeSalary();
+    }
+    if(employeeType === employeeTypes.COMMISSIONED){
+        return calculateCommissionedEmployeeSalary(salesPerMonth);
+    }
+    if(employeeType === employeeTypes.CONTRACT){
+        return calculateContractEmployeeSalary(salesPerMonth);
+    }
+
+    return "Oops! There was an error. We'll get back to you shortly"
+}
+
+console.log(calculateEmployeeSalary('SALARIED', 880))
+
+
+
+
+
